47d0b699b3b43a0a9820496ffc1a2d9dcb93a0a6
mauricebutler <maurice.butler@gmail.com>
2014-08-20 10:16:06 +1000

    added licence, dodgey fix for trailing e and uppercase support

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 module.exports = function ify(){
-	var args = Array.prototype.slice.call(arguments);
+	var args = Array.prototype.slice.call(arguments),
+    result = args.join(' ');
 
-	return args.join(' ') + 'ify';
+    if(result[result.length -1].toLowerCase() === 'e'){
+        result = result.substring(0, result.length -1);
+    }
+
+    if(result === result.toUpperCase()){
+        return result + 'IFY';
+    } else {
+        return result + 'ify';
+    }
 };
